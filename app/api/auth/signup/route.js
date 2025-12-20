3f359b6c35c04afc3038f34996be7cdbe631f8c8
Saroj_Dev <sarojtech0005@gmail.com>
2025-12-20 10:11:46 +0530

    Added user signup api

---
+++ app/api/auth/signup/route.js
@@ -0,0 +1,38 @@
+import { connectDB } from "@/app/backend/db/connect";
+import bcrypt from "bcryptjs";
+import User from "@/app/backend/models/User.model"
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { name, email, password } = await req.json();
+
+  if (!name || !email || !password) {
+    return NextResponse.json(
+      { message: "All fields required" },
+      { status: 400 }
+    );
+  }
+
+  await connectDB();
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return NextResponse.json(
+      { message: "User already exists" },
+      { status: 409 }
+    );
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  return NextResponse.json(
+    { message: "Signup successful" },
+    { status: 201 }
+  );
+}
---
+++ app/api/auth/users/me/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { verifyToken } from "@/lib/jwt";
+import User from "@/backend/models/User.model";
+import { connectDB } from "@/app/backend/db/connect";
+
+export async function GET() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.json(
+      { message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const decoded = verifyToken(token);
+
+    await connectDB();
+    const user = await User.findById(decoded.id).select("-password");
+
+    return NextResponse.json(user);
+  } catch {
+    return NextResponse.json(
+      { message: "Invalid token" },
+      { status: 401 }
+    );
+  }
+}
